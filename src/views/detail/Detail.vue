9644b185bef35e0a6de8e4f6580f95299c420d68
laixingyou <1156663717@qq.com>
2020-03-28 16:27:02 +0800

    1

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,26 +1,90 @@
 <template>
   <div id="detail">
-    <detail-nav></detail-nav>
+    <detail-nav class="detail-nav"></detail-nav>
+
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+      <detail-param-info :param-info="paramInfo" />
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNav from "./childComps/DetailNav";
+import DetailSwiper from "./childComps/DetailSwiper";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import DetailShopInfo from "./childComps/DetailShopInfo";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+import DetailParamInfo from "./childComps/DetailParamInfo";
+import Scroll from "components/common/scroll/Scroll";
+
+import { getDetail, Goods, Shop, GoodsParam } from "network/detail";
 
 export default {
   name: "Detail",
   components: {
-    DetailNav
+    DetailNav,
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    Scroll
   },
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {}
     };
   },
   created() {
     this.iid = this.$route.params.iid;
+    getDetail(this.iid).then(res => {
+      // console.log(res);
+      const data = res.result;
+
+      this.topImages = data.itemInfo.topImages;
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+      this.shop = new Shop(data.shopInfo);
+      this.detailInfo = data.detailInfo;
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
+    });
+  },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh();
+    }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+.content {
+  height: calc(100% - 44px);
+}
+</style>
