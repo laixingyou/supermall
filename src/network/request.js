50246bde8b9ddd38766ce8dd092b37ed6fbe044d
laixingyou <1156663717@qq.com>
2020-04-24 15:33:19 +0800

    .

--- src/network/request.js
+++ src/network/request.js
@@ -7,20 +7,20 @@ export function request(config) {
     timeout: 5000
   })
 
-  // 2.axios的拦截器
-  // 2.1.请求拦截的作用
-  instance.interceptors.request.use(config => {
-    return config
-  }, err => {
-    // console.log(err);
-  })
+  // // 2.axios的拦截器
+  // // 2.1.请求拦截的作用
+  // instance.interceptors.request.use(config => {
+  //   return config
+  // }, err => {
+  //   // console.log(err);
+  // })
 
-  // 2.2.响应拦截
-  instance.interceptors.response.use(res => {
-    return res.data
-  }, err => {
-    console.log(err);
-  })
+  // // 2.2.响应拦截
+  // instance.interceptors.response.use(res => {
+  //   return res.data
+  // }, err => {
+  //   console.log(err);
+  // })
 
   // 3.发送真正的网络请求
   return instance(config)
